a9dee7a3faa82b9e25615215b2c9cf3ec36f485f
Samarpan Jain <68655137+samarpan-jain@users.noreply.github.com>
2024-07-04 03:51:00 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
   await app.listen(3000);
 }
 bootstrap();
