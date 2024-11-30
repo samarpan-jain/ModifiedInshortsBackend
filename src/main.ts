86f2e6133b4d486b5c83604b9d0fe4f08f12212f
Samarpan Jain <samarpanjain29@gmail.com>
2024-12-01 00:06:02 +0530

    updated file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', // Allow specific origin
+    origin: 'https://modified-inshorts-clone.vercel.app', // Allow specific origin
     methods: 'GET,POST,PUT,DELETE',            // Allowed HTTP methods
     allowedHeaders: 'Content-Type,Authorization',
   });
