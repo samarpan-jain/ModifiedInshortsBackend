8f12a20bbeb5fc7f63f75933e0c1a29d90af4641
Samarpan Jain <samarpanjain29@gmail.com>
2024-12-01 00:20:34 +0530

    Cors Update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsMiddleware } from './middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',  // Allow all origins (not safe for production)
-    methods: 'GET,POST,PUT,DELETE',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: false
   });
+  app.use(new CorsMiddleware().use);
   await app.listen(3000);
 }
 bootstrap();
