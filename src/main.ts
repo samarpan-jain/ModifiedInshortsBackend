3c66431cf57d0eaf0bae6bbab94d94a0245c676f
Samarpan Jain <samarpanjain29@gmail.com>
2024-12-01 00:07:04 +0530

    new Update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'https://modified-inshorts-clone.vercel.app', // Allow specific origin
-    methods: 'GET,POST,PUT,DELETE',            // Allowed HTTP methods
+    methods: ['GET','POST','PUT','DELETE'],            // Allowed HTTP methods
     allowedHeaders: 'Content-Type,Authorization',
   });
   await app.listen(3000);
